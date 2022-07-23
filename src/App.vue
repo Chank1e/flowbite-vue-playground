9006a6e09782c3ecb89edebeff03c84683a178d4
Alexandr <a.pashkevich22@gmail.com>
2022-07-23 19:37:29 +0300

    feat: initial

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex gap-10">
+    <Button :color="'green'">Example</Button>
+    <Button :color="'red'">Example</Button>
+    <Button :color="'purple'">Example</Button>
+    <dropdown text="Нажми на меня" placement="top">
+      <list-group>
+        <list-group-item>Example</list-group-item>
+      </list-group>
+    </dropdown>
+  </div>
+</template>
+<script setup lang="ts">
+import { Button, Dropdown, ListGroup, ListGroupItem } from 'flowbite-vue'
+</script>
